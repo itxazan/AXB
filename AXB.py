4bcad07c56c15e4f523bb64897da9b5fc702ae92
itxazan <156661331+itxazan@users.noreply.github.com>
2024-02-10 21:34:58 +0500

    AXB.py

--- AXB.py
+++ AXB.py
@@ -716,7 +716,8 @@ def YounisXyz(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            xyzheader_fuck = {'authority': 'free.facebook.com',
+            xyzheader_fuck = {
+    'authority': 'free.facebook.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
     'cache-control': 'max-age=0',
